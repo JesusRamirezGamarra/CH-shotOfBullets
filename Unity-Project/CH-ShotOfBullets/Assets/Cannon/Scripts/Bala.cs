85c1676f6453c60befb66bfba4d25ac262fc2c1a
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2022-10-12 06:30:59 -0500

    Deploy

--- Unity-Project/CH-ShotOfBullets/Assets/Cannon/Scripts/Bala.cs
+++ Unity-Project/CH-ShotOfBullets/Assets/Cannon/Scripts/Bala.cs
@@ -4,7 +4,7 @@ public class Bala : MonoBehaviour
 {
     public float speed = .25f;
     public Vector3 direction =  new Vector3(1,1,1);
-    public int damege = 0;
+    public int damage = 0;
 
     
     // Start is called before the first frame update
--- Unity-Project/CH-ShotOfBullets/Assets/Cannon/Scripts/Cannon.cs
+++ Unity-Project/CH-ShotOfBullets/Assets/Cannon/Scripts/Cannon.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Cannon : MonoBehaviour
 {
 	public GameObject objetoAInstanciar;
     public Transform lugar;
+	private bool IsAvailable = true;
+	private float CooldownDuration = 1.0f;	
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Shoot();
-		}
+        if (IsAvailable &&  Input.GetKeyDown(KeyCode.Space))
+        {		
+				Shoot();
+		}			
     }
 	
 	private void Shoot()
 	{
 		Debug.Log("The Cannon has been shoot!");
 		Instantiate(objetoAInstanciar,lugar.position,transform.rotation);
+		StartCoroutine(StartCooldown());		
 	}
+
+	public IEnumerator StartCooldown()
+	{
+		IsAvailable = false;
+		yield return new WaitForSeconds(CooldownDuration);
+		IsAvailable = true;
+	}
+
 }
